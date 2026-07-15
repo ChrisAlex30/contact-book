c5678a5c64cddf085a39f545c62e880ec7b5e031
ChrisAlex30 <akshara155301@gmail.com>
2026-07-15 15:51:02 +0530

    updated getContacts handler

--- backend/src/handlers/getContacts.ts
+++ backend/src/handlers/getContacts.ts
@@ -1,5 +1,4 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
-
 import {
   GetObjectCommand,
   S3Client,
@@ -7,55 +6,94 @@ import {
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 import { connectDB } from "../db/connect.js";
-import { Contact } from "../models/Contact.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Contact } from "../models/Contact.js";
 
-const s3 = new S3Client({
-  region: "ap-south-1",
-});
+const s3 = new S3Client({region: "ap-south-1"});
+const allowedSortFields = [
+  "name",
+  "email",
+  "phone",
+  "createdAt",
+] as const;
 
-const getContact = async (
-  event: APIGatewayProxyEvent
-) => {
+
+const getContacts = async (event: APIGatewayProxyEvent) => {
 
   await connectDB();
+  const userId = (event as any).requestContext.authorizer.jwt.claims.sub;
+  const page = Math.max(1,Number(event.queryStringParameters?.page) || 1);
+  const limit = Math.min(50,Math.max(1,Number(event.queryStringParameters?.limit) || 10));
+  const skip = (page - 1) * limit;
 
-  const userId =
-    (event as any).requestContext.authorizer.jwt.claims.sub;
+  const search = event.queryStringParameters?.search?.trim() ?? "";
 
-  const id = event.pathParameters?.id;
-
-  const contact = await Contact.findOne({
-    _id: id,
+  const filter: any = {
     userId,
-  });
-
-  if (!contact) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({
-        message: "Contact not found.",
-      }),
-    };
+  };
+  if (search) {
+  filter.$or = [
+    {
+      name: {
+        $regex: search,
+        $options: "i",
+      },
+    },
+    {
+      email: {
+        $regex: search,
+        $options: "i",
+      },
+    },
+    {
+      phone: {
+        $regex: search,
+        $options: "i",
+      },
+    },
+  ];
   }
 
-  const imageUrls = await Promise.all(
-    contact.images.map(async (key: string) => {
+  const requestedSort =
+  event.queryStringParameters?.sort ?? "-createdAt";
 
-      const command = new GetObjectCommand({
-        Bucket: process.env.BUCKET_NAME,
-        Key: key,
-      });
+  const isDescending =
+    requestedSort.startsWith("-");
+
+  const sortField = isDescending
+    ? requestedSort.substring(1)
+    : requestedSort;
+
+  const finalSortField = allowedSortFields.includes(sortField as any) ? sortField : "createdAt";
+
+  const sortOption:Record<string, 1 | -1> = {
+    [finalSortField]: isDescending ? -1 : 1,
+  };
+
+  const[total,contacts]=await Promise.all([
+    Contact.countDocuments(filter),
+    Contact.find(filter)
+    .sort(sortOption)
+    .skip(skip)
+    .limit(limit)
+  ])
+
+
+  const contactsWithImages = await Promise.all(
+    contacts.map(async (contact) => {
+      const imageUrls = await Promise.all(
+        contact.images.map(async (key: string) => {
+          const command = new GetObjectCommand({Bucket: process.env.BUCKET_NAME,Key: key});
+          return {
+            key,
+            url: await getSignedUrl(s3,command,{expiresIn: 3600})
+          };
+        })
+      );
 
       return {
-        key,
-        url: await getSignedUrl(
-          s3,
-          command,
-          {
-            expiresIn: 3600,
-          }
-        ),
+        ...contact.toObject(),
+        imageUrls,
       };
     })
   );
@@ -63,11 +101,16 @@ const getContact = async (
   return {
     statusCode: 200,
     body: JSON.stringify({
-      ...contact.toObject(),
-      imageUrls,
-    }),
+    contacts: contactsWithImages,
+    pagination: {
+      page,
+      limit,
+      total,
+      totalPages: Math.ceil(total / limit),
+    },
+  })
   };
 };
 
 export const handler =
-  asyncHandler(getContact);
+  asyncHandler(getContacts);
